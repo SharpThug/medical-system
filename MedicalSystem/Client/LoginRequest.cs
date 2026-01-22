c3d00cd055204c81e2341a7ff7bb37121bc78dde
JavaThugNGG <ildarjava@mail.ru>
2026-01-22 18:12:56 +0300

    api: fix(Auth): add JSON propery mappings to LoginRequest

--- MedicalSystem/Client/LoginRequest.cs
+++ MedicalSystem/Client/LoginRequest.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Client
 {
     public class LoginRequest
     {
+        [JsonPropertyName("login")]
         public string Login { get; set; } = string.Empty;
+        [JsonPropertyName("password")]
         public string Password { get; set; } = string.Empty;
     }
 }
