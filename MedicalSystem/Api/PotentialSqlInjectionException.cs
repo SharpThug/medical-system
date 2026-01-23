7395c2f9ff7ff076c8df775d6ae5a2ead94d3dbb
JavaThugNGG <ildarjava@mail.ru>
2026-01-23 15:15:39 +0300

    api: chore(Auth): improve log message clarity

--- MedicalSystem/Api/PotentialSqlInjectionException.cs
+++ MedicalSystem/Api/PotentialSqlInjectionException.cs
@@ -3,7 +3,7 @@
     public class PotentialSqlInjectionException : Exception
     {
         public PotentialSqlInjectionException(string tableName)
-            : base($"Обнаружена попытка недопустимого имени таблицы: {tableName}")
+            : base($"Обнаружена попытка недопустимого имени таблицы при ее создании: {tableName}")
         { }
     }
 }
