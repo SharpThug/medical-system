117badbf80803425b8b9fb4b892f2c77dc9c68ab
JavaThugNGG <ildarjava@mail.ru>
2026-01-27 19:26:33 +0300

    client: feat(service): add PatientService for pulling patients from api

---
+++ MedicalSystem/Client/IPatientService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public interface IPatientService
+    {
+        public Task<List<Patient>> GetPatientsAsync(int count);
+    }
+}
---
+++ MedicalSystem/Client/PatientService.cs
@@ -0,0 +1,40 @@
+﻿using Shared;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class PatientService : IPatientService
+    {
+        private readonly HttpClient _httpClient;
+
+
+        public PatientService(HttpClient httpClient)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<List<Patient>> GetPatientsAsync(int count)
+        {
+            try
+            {
+                string apiUrl = $"https://localhost:7218/api/patient?count={count}";
+
+                // Парсим ApiResponse<List<Patient>>
+                var response = await _httpClient.GetFromJsonAsync<ApiResponse<List<Patient>>>(apiUrl);
+
+                if (response == null || !response.Success)
+                    return new List<Patient>();
+
+                return response.Data ?? new List<Patient>();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ошибка при получении пациентов с API: " + ex.Message, ex);
+            }
+        }
+    }
+}
