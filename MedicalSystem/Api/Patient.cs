7c07c92fbaa06991e1082a7c98c66a7f544509d7
JavaThugNGG <ildarjava@mail.ru>
2026-01-27 19:20:09 +0300

    api: fix(patient): update Patient class to match all database fields

---
+++ MedicalSystem/Api/Patient.cs
@@ -0,0 +1,19 @@
+﻿namespace Api
+{
+    public class Patient
+    {
+        public string CardNumber { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string? Patronymic { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string? Phone { get; set; }
+        public string Gender { get; set; } = null!;
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+        public string? Allergies { get; set; }
+        public string? ChronicDiseases { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+    }
+}
