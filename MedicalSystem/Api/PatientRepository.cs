696a7926442488cc9bf65ece642f911fb4c4349b
JavaThugNGG <ildarjava@mail.ru>
2026-01-28 16:19:58 +0300

    api: refactor(repository): add Id and OmsNumber fields to Patient mapping

--- MedicalSystem/Api/PatientRepository.cs
+++ MedicalSystem/Api/PatientRepository.cs
@@ -19,28 +19,29 @@ namespace Api
 
             var patients = new List<Patient>();
 
-            string sql = @"
-        SELECT TOP (@Count) 
-               CardNumber, 
-               LastName, 
-               FirstName, 
-               Patronymic, 
-               BirthDate, 
-               Phone, 
-               Gender, 
-               Address, 
-               Email, 
-               Allergies, 
-               ChronicDiseases, 
-               CreatedDate
-        FROM Patients
-        ORDER BY CreatedDate DESC
-    ";
-
             await using var connection = new SqlConnection(_connectionString);
-            await using var command = new SqlCommand(sql, connection);
+            await using var command = new SqlCommand(
+                """
+                SELECT TOP (@Count) 
+                    Id,
+                    CardNumber, 
+                    OmsNumber,
+                    LastName, 
+                    FirstName, 
+                    Patronymic, 
+                    BirthDate, 
+                    Phone, 
+                    Gender, 
+                    Address, 
+                    Email, 
+                    Allergies, 
+                    ChronicDiseases, 
+                    CreatedDate
+                FROM Patients
+                ORDER BY CreatedDate DESC
+                """, 
+                connection);
 
-            // Параметры всегда лучше явно указывать тип
             command.Parameters.Add("@Count", System.Data.SqlDbType.Int).Value = count;
 
             await connection.OpenAsync();
@@ -50,32 +51,21 @@ namespace Api
             {
                 var patient = new Patient
                 {
+                    Id = reader.GetInt64(reader.GetOrdinal("Id")),
                     CardNumber = reader.GetString(reader.GetOrdinal("CardNumber")),
+                    OmsNumber = reader["OmsNumber"] as string,
                     LastName = reader.GetString(reader.GetOrdinal("LastName")),
                     FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
-                    Patronymic = reader.IsDBNull(reader.GetOrdinal("Patronymic"))
-                                 ? null
-                                 : reader.GetString(reader.GetOrdinal("Patronymic")),
+                    Patronymic = reader.GetString(reader.GetOrdinal("Patronymic")),
                     BirthDate = reader.GetDateTime(reader.GetOrdinal("BirthDate")),
-                    Phone = reader.IsDBNull(reader.GetOrdinal("Phone"))
-                            ? null
-                            : reader.GetString(reader.GetOrdinal("Phone")),
                     Gender = reader.GetString(reader.GetOrdinal("Gender")),
-                    Address = reader.IsDBNull(reader.GetOrdinal("Address"))
-                              ? null
-                              : reader.GetString(reader.GetOrdinal("Address")),
-                    Email = reader.IsDBNull(reader.GetOrdinal("Email"))
-                            ? null
-                            : reader.GetString(reader.GetOrdinal("Email")),
-                    Allergies = reader.IsDBNull(reader.GetOrdinal("Allergies"))
-                                ? null
-                                : reader.GetString(reader.GetOrdinal("Allergies")),
-                    ChronicDiseases = reader.IsDBNull(reader.GetOrdinal("ChronicDiseases"))
-                                      ? null
-                                      : reader.GetString(reader.GetOrdinal("ChronicDiseases")),
+                    Phone = reader["Phone"] as string,
+                    Address = reader["Address"] as string,
+                    Email = reader["Email"] as string,
+                    Allergies = reader["Allergies"] as string,
+                    ChronicDiseases = reader["ChronicDiseases"] as string,
                     CreatedDate = reader.GetDateTime(reader.GetOrdinal("CreatedDate"))
                 };
-
                 patients.Add(patient);
             }
             return patients;
